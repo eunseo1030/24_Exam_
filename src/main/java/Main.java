d1dca3dabfcc44706a01e4923bd2d66c72e0a86d
eunseo1030 <eunseo20001030@gmail.com>
2024-06-27 13:44:13 +0900

    프로젝트세팅

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("3*4");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
