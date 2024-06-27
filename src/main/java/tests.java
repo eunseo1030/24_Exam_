81e535b701e8b3b7c96e41087a64f4b687bdefae
eunseo1030 <eunseo20001030@gmail.com>
2024-06-27 14:43:01 +0900

    2번

--- src/main/java/tests.java
+++ src/main/java/tests.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
-
-public class Main {
+public class tests {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 5;
 
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) {
-                System.out.print(" ");
+                System.out.print("n");
             }
             for (int j = 1; j <=(2*i-1); j++) {
 
@@ -16,13 +13,5 @@ public class Main {
             }
             System.out.println();
         }
-
-
     }
 }
-
-
-
-
-
-
