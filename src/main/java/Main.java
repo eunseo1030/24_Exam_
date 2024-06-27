c403cf26a4665fae93e84fd4fce8367665d77240
eunseo1030 <eunseo20001030@gmail.com>
2024-06-27 13:52:03 +0900

    1번문제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("3*4");
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        System.out.println();
+
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
+
+
+
+
 }
+
+
 
 
 
