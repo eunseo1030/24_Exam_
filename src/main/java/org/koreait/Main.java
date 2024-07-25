f1ea8cccedca1f752f69b8af80045ff3a08de0dc
eunseo1030 <eunseo20001030@gmail.com>
2024-07-25 17:02:36 +0900

    1번문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,21 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
